687e584be794eaae33da25d4144a2c080176b781
nuruddin-99 <nuruddinalways99@gmail.com>
2021-08-28 23:02:09 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Selector
+const logBtn = document.getElementById("logBtn");
+const logArea = document.getElementById("login-area");
+const transactionArea = document.getElementById("transaction");
+
+// Login Handler
+logBtn.addEventListener("click", () => {
+  logArea.style.display = "none";
+  transactionArea.style.display = "block";
+});
+// Deposit Button Handler
+
+const depositBtn = document
+  .getElementById("deopBtn")
+  .addEventListener("click", () => {
+    
+  });
